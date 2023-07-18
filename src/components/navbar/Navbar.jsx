f21631b189989d987c29d4b609a54ea35b2290cf
Syasyazman <syasyazman2000@gmail.com>
2023-07-18 17:26:45 +0800

    Implement home page and event flow page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Blackjack from "../../pages/blackjack/Blackjack";
-import Uno from "../../pages/uno/Uno";
-import Suitpoints from "../../pages/suitpoints/Suitpoints";
-import Lineup from "../../pages/lineup/Lineup";
 import './navbar.css';
 
 const Navbar = () => {
     return (
         <div className="header__navbar">
-            <NavLink exact className={({isActive}) => isActive ? "header__navbar-link--active" : "header__navbar-link" } to="/blackjack">
-                <p>Blackjack Poker Gameplay</p>
-            </NavLink>
-            <NavLink exact className={({isActive}) => isActive ? "header__navbar-link--active" : "header__navbar-link" } to="/uno">
-                <p>Uno Advantages</p>
+            <NavLink exact className={({isActive}) => isActive ? "header__navbar-link--active" : "header__navbar-link" } to="/eventflow">
+                <p>Event Flow</p>
             </NavLink>
             <NavLink exact className={({isActive}) => isActive ? "header__navbar-link--active" : "header__navbar-link" } to="/suitpoints">
                 <p>Top 32 Point System</p>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import dbgIgLogo from '../../assets/dbg_ig.png'
+import dhopsIgLogo from '../../assets/dhops_ig.png'
+import dhopsYtLogo from '../../assets/dhops_yt.png'
+import thriftIgLogo from '../../assets/thrift_ig.png'
+import './home.css'
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="home">
+            <div className="home__text">
+                <p className="home__text-first">WELCOME!!</p>
+                <p className="home__text-second">FEEL FREE TO FOLLOW OUR SOCIALS</p>
+            </div>
+            <div className="home__logo-container">
+                <div className="home__logo">
+                    <img src={dbgIgLogo} className="home__logo-img" />
+                    <p className="home__logo-text">d'Battleground IG</p>
+                </div>
+                <div className="home__logo">
+                    <img src={dhopsIgLogo} className="home__logo-img" />
+                    <p className="home__logo-text">NUS d'Hoppers IG</p>
+                </div>
+                <div className="home__logo">
+                    <img src={dhopsYtLogo} className="home__logo-img" />
+                    <p className="home__logo-text">NUS d'Hoppers Youtube</p>
+                </div>
+                <div className="home__logo">
+                    <img src={thriftIgLogo} className="home__logo-img" />
+                    <p className="home__logo-text">Thrifthoppers IG</p>
+                </div>
+            </div>
         </div>
     )
 }
