dbf645e05ab45c73d9dc50a27bffe6ca36a91570
Syasyazman <syasyazman2000@gmail.com>
2023-07-22 15:11:21 +0800

    Clean up aesthetics

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,14 @@ import dbgIgLogo from '../../assets/dbg_ig.png'
 import dhopsIgLogo from '../../assets/dhops_ig.png'
 import dhopsYtLogo from '../../assets/dhops_yt.png'
 import thriftIgLogo from '../../assets/thrift_ig.png'
+import dbgBackground from '../../assets/dbg_background.png'
+import dbgTeleLogo from '../../assets/dbg_tele.png'
 import './home.css'
 
 const Home = () => {
     return (
         <div className="home">
+            <img src={dbgBackground} className="home-background" />
             <div className="home__text">
                 <p className="home__text-first">WELCOME!!</p>
                 <p className="home__text-second">FEEL FREE TO FOLLOW OUR SOCIALS</p>
@@ -29,6 +32,12 @@ const Home = () => {
                     <img src={thriftIgLogo} className="home__logo-img" />
                     <p className="home__logo-text">Thrifthoppers IG</p>
                 </div>
+            </div>
+            <div className="home__telegram-container">
+                <div className="home__text home__text-third">
+                    <p>Have you joined d'Battleground's telegram channel?</p>
+                </div>
+                <img src={dbgTeleLogo} className="home__logo-img" />
             </div>
         </div>
     )
--- src/pages/lineup/Lineup.jsx
+++ src/pages/lineup/Lineup.jsx
@@ -8,6 +8,14 @@ import dbgLogo from '../../assets/dbg_logo.png';
 import dbgLineupBackground from '../../assets/dbg_lineup_background.png';
 import './lineup.css';
 
+const textLineupStyle = {
+    color: "white",
+    textAlign: "center",
+    fontFamily: "Orbitron, Roboto, Verdana",
+    fontWeight: "bold",
+    fontSize: "16px"
+}
+
 const Lineup = (props) => {
     const [refreshLineup, setRefreshLineup] = useState(false)
 
@@ -18,11 +26,11 @@ const Lineup = (props) => {
                 <img src={dbgLineupBackground} className="lineup__background-tree" />
             </div>
             <div className="lineup__header">
-                <button className="lineup__header-buttons" onClick={() => {randomizeLineup(props.topEightList, true); setRefreshLineup((prev) => !prev);}}>
-                    Randomize Top Eight
-                </button>
                 <button className="lineup__header-buttons" onClick={() => {randomizeLineup(props.topFourList, false); setRefreshLineup((prev) => !prev);}}>
                     Randomize Top Four
+                </button>
+                <button className="lineup__header-buttons" onClick={() => {randomizeLineup(props.topEightList, true); setRefreshLineup((prev) => !prev);}}>
+                    Randomize Top Eight
                 </button>
             </div>
             <div className="lineup__content">
@@ -32,6 +40,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" " 
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-eight"
                                 value={props.topEightList[0].name}
                                 onChange={(event) => {changeNameInLineup(0, event.target.value, props.topEightList, "eight"); setRefreshLineup((prev) => !prev);}}
@@ -41,6 +51,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" " 
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-four"
                                 value={props.topFourList[0].name}
                                 onChange={(event) => {changeNameInLineup(0, event.target.value, props.topFourList, "four"); setRefreshLineup((prev) => !prev);}}
@@ -50,6 +62,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" " 
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-two"
                                 value={props.topTwoList[0].name}
                                 onChange={(event) => {changeNameInLineup(0, event.target.value, props.topTwoList, "two"); setRefreshLineup((prev) => !prev);}}
@@ -59,6 +73,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" "
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-two"
                                 value={props.topTwoList[1].name}
                                 onChange={(event) => {changeNameInLineup(1, event.target.value, props.topTwoList, "two"); setRefreshLineup((prev) => !prev);}}
@@ -68,6 +84,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" "
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-four"
                                 value={props.topFourList[2].name}
                                 onChange={(event) => {changeNameInLineup(2, event.target.value, props.topFourList, "four"); setRefreshLineup((prev) => !prev);}}
@@ -77,6 +95,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" "
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-eight"
                                 value={props.topEightList[4].name}
                                 onChange={(event) => {changeNameInLineup(4, event.target.value, props.topEightList, "eight"); setRefreshLineup((prev) => !prev);}}
@@ -88,6 +108,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" " 
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-eight"
                                 value={props.topEightList[1].name}
                                 onChange={(event) => {changeNameInLineup(1, event.target.value, props.topEightList, "eight"); setRefreshLineup((prev) => !prev);}}
@@ -97,6 +119,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" " 
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-eight"
                                 value={props.topEightList[5].name}
                                 onChange={(event) => {changeNameInLineup(5, event.target.value, props.topEightList, "eight"); setRefreshLineup((prev) => !prev);}}
@@ -108,6 +132,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" " 
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-eight"
                                 value={props.topEightList[2].name}
                                 onChange={(event) => {changeNameInLineup(2, event.target.value, props.topEightList, "eight"); setRefreshLineup((prev) => !prev);}}
@@ -117,6 +143,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" " 
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-four"
                                 value={props.topFourList[1].name}
                                 onChange={(event) => {changeNameInLineup(1, event.target.value, props.topFourList, "four"); setRefreshLineup((prev) => !prev);}}
@@ -126,6 +154,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" " 
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-four"
                                 value={props.topFourList[3].name}
                                 onChange={(event) => {changeNameInLineup(3, event.target.value, props.topFourList, "four"); setRefreshLineup((prev) => !prev);}}
@@ -135,6 +165,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" " 
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-eight"
                                 value={props.topEightList[6].name}
                                 onChange={(event) => {changeNameInLineup(6, event.target.value, props.topEightList, "eight"); setRefreshLineup((prev) => !prev);}}
@@ -146,6 +178,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" " 
                                 InputLabelProps={{shrink: false}}
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-eight"
                                 value={props.topEightList[3].name}
                                 onChange={(event) => {changeNameInLineup(3, event.target.value, props.topEightList, "eight"); setRefreshLineup((prev) => !prev);}}
@@ -155,6 +189,8 @@ const Lineup = (props) => {
                             <TextField 
                                 label=" " 
                                 InputLabelProps={{shrink: false}} 
+                                multiline={true}
+                                inputProps={{ style:textLineupStyle }}
                                 className="lineup-text top-eight"
                                 value={props.topEightList[7].name}
                                 onChange={(event) => {changeNameInLineup(7, event.target.value, props.topEightList, "eight"); setRefreshLineup((prev) => !prev);}}
